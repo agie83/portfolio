f751d4b6def7f3ff12143ba1db66321321fd5831
agie83 <agnes.agocs@gmail.com>
2023-01-04 14:50:33 +0100

    delete unnecessary row

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import React from "react";
 
 const ProjectItem = ({ title, backgroundImg, tech, projectUrl, place }) => {
-  //const imgPath = "http://aeweb.hu/portfolio-images/";
   const imgPath = "/assets/projects/";
   const fontSize = place === "menuPage" ? "text-lg" : "text-2xl";
   const buttonTextSize = place === "menuPage" ? "text-sm" : "text-lg";
